e3bff7731bec82cc692b097788c62b215c27df33
techhirestudent <live_work_achieve@outlook.com>
2017-02-09 23:55:44 -0500

    Review Ex 2

---
+++ ReviewEx2.rb
@@ -0,0 +1,14 @@
+# hash or pound or octothorpe "comments out"
+
+#$ ruby ex2.rb
+#I could have code like this.
+#This will run.
+
+#comment test
+
+puts "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
+
+puts "This will run."
