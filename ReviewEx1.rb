84e535f4f950e7dec61c5f1140c3c49bf0f8920b
techhirestudent <live_work_achieve@outlook.com>
2017-02-09 23:45:08 -0500

    review

---
+++ ReviewEx1.rb
@@ -0,0 +1,12 @@
+#Ex:0-->The set up
+#install Atom-- completed
+
+#Review Ex1.rb
+
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
